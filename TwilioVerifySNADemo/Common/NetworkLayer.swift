f106c6b150980dae19fb8446527beb4c8e0e6cfd
Carlos Mejía <carlosmejia083@gmail.com>
2022-07-26 15:06:31 -0500

    feat(26157): demo app created
    * [26157] Demo app created
    
    * [26157] Docs improved
    
    * [26157] PR comments addressed
    
    * [26157] PR comments addressed
    
    * [26157] PR comments addressed
    
    * [26157] Country code input fixed
    
    * [26157] Fixing SwiftLint script on Xcode 14

---
+++ TwilioVerifySNADemo/Common/NetworkLayer.swift
@@ -0,0 +1,55 @@
+//
+//  NetworkLayer.swift
+//  TwilioVerifySNADemo
+//
+//  Copyright © 2022 Twilio.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+import Foundation
+
+/**
+ This network layer is created with the only purpose of helping the demo app to perform network requests.
+ In the best practices you probably use your own networking layer or third parties like Alamofire.
+ */
+final class NetworkLayer {
+    func post<Response: Decodable, Request: Encodable>(
+        to url: String,
+        request: Request,
+        onComplete: @escaping (Response?) -> Void
+    ) {
+        guard let url = URL(string: url) else {
+            return onComplete(nil)
+        }
+
+        var urlRequest = URLRequest(url: url)
+        urlRequest.httpMethod = "POST"
+        urlRequest.httpBody = try? JSONEncoder().encode(request)
+
+        URLSession.shared.dataTask(with: urlRequest) { data, _, error in
+            guard let data = data, error == nil else {
+                onComplete(nil)
+                return
+            }
+
+            guard let convertedResponse = try? JSONDecoder().decode(Response.self, from: data) else {
+                onComplete(nil)
+                return
+            }
+
+            onComplete(convertedResponse)
+
+        }.resume()
+    }
+}
